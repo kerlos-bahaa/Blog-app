351dad970ee6b2b714bea46daa0292bc3019951b
kerlosbahaa3 <aboelking4@gmail.com>
2023-10-13 07:12:19 +0300

    Views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    page = params[:page] || 1
+    per_page = 10
 
-  def show; end
+    @posts = Post.includes(:author)
+      .includes(:comments)
+      .where(author: params[:user_id])
+      .order(created_at: :asc)
+      .offset((page.to_i - 1) * per_page)
+      .limit(per_page)
+
+    @total_pages = (Post.count.to_f / per_page).ceil
+    @author = @posts.first.author
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
-  def three_most_recent_posts
+  def three_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
